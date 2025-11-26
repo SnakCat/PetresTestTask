483f8e412fe0fd8dcfa012e8d27ba58de5443a0e
SnakCat <snakkdss@gmail.com>
2025-11-26 16:17:04 +0300

    initial commit

---
+++ PetresTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PetresTestTask
+//
+//  Created by DimaTru on 26.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
